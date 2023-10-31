c65c1698af93af4ab682f91db047aac5f0df54d2
Salil Agrawal <56884743+ersalil@users.noreply.github.com>
2023-10-31 17:56:42 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://uprevol:live12345@horizonhold.cs3vggqcgesi.ap-south-1.rds.amazonaws.com:5432/horizonhold"
+DATABASE_URL = "postgresql://uprevol:live12345@horizonhold2.cs3vggqcgesi.ap-south-1.rds.amazonaws.com:5432/horizonhold2"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
